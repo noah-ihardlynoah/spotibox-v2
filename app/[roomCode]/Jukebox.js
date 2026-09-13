fe158865a7226b7cee9fa98e45888849e4176817
noah-ihardlynoah <noahjmassie09@gmail.com>
2026-09-14 00:49:21 +0100

    fixed play button greyout error

--- app/[roomCode]/Jukebox.js
+++ app/[roomCode]/Jukebox.js
@@ -754,7 +754,6 @@ export default function Jukebox({ roomCode, session }) {
                           <button
                             type="button"
                             onClick={() => playTrack(index)}
-                            disabled={index === queue.length - 1}
                             aria-label="Play"
                             title="Play"
                           >
